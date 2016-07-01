0a285f37df00e5c836579453dbddcc0685ece9d3
Tony Galvão <webtals@gmail.com>
2016-07-01 14:22:27 -0300

    Perfil Usuário
    Iniciando a construção do perfil dos usuários

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Mongo } from 'meteor/mongo';
 
 import { name as simShop } from '../imports/ui/components/simshop/simShop';
+
+export const nomeDoSistema = "Sim Shop v0.1"; //O nome aqui colocado será replicado em todo o sistema
+
+
+document.title = nomeDoSistema;
+export const Produto = new Mongo.Collection('produtos');
